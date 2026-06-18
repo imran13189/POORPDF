015bf1bb72535b2ff16451905a685bbc13d14ebc
imran13189 <imran13189@gmail.com>
2026-06-18 10:15:35 +0530

    tagmanger

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using WebMarkupMin.AspNetCore6;
 
-
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddWebMarkupMin(options =>
+{
+    options.AllowMinificationInDevelopmentEnvironment = true; // Only minify in production
+})
+.AddHtmlMinification();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
@@ -10,6 +16,12 @@ builder.Services.AddEndpointsApiExplorer();
 
 
 var app = builder.Build();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+app.UseHttpsRedirection();
+app.UseWebMarkupMin();
 
 app.UseStaticFiles();
 
